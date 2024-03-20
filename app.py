13ab07d1ebcbdba6c1c9ad1c334ab8cd9496fdcb
inderkiranbhamra <140848814+inderkiranbhamra@users.noreply.github.com>
2024-03-20 23:03:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,17 +11,17 @@ CORS(app)
 app.secret_key = 'inderkiran@24'
 
 # MySQL database configuration
-DB_HOST = 'sql6.freesqldatabase.com'
-DB_NAME = 'sql6690830'
-DB_USER = 'sql6690830'
-DB_PASSWORD = 'elmCpSuTy2'
+DB_HOST = '217.21.94.103'
+DB_NAME = 'u813060526_gameathonregis'
+DB_USER = 'u813060526_gameathonregis'
+DB_PASSWORD = '135@Hack'
 
 # Connect to the MySQL database
 conn = mysql.connector.connect(host=DB_HOST, database=DB_NAME, user=DB_USER, password=DB_PASSWORD)
 cursor = conn.cursor()
 
-# cursor.execute("CREATE TABLE IF NOT EXISTS UniqueIGN (ign VARCHAR(255) UNIQUE)")
-# conn.commit()
+cursor.execute("CREATE TABLE IF NOT EXISTS UniqueIGN (ign VARCHAR(255) UNIQUE)")
+conn.commit()
 # cursor.execute("DROP TABLE IF EXISTS UniqueIGN")
 # conn.commit()
 cursor.execute('''CREATE TABLE IF NOT EXISTS BGMIregistrations (
